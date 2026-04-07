34ebbd4d1413cfae7223623309c9f155913b0a53
Walker-Gideon <gideonawortwe3@gmail.com>
2026-04-07 04:06:56 +0000

    Created the register route file

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,6 +3,7 @@ import { Router } from "express";
 import helpRouter from "./help.js";
 import taskRouter from "./task.js";
 import settingsRouter from "./settings.js";
+import registerRouter from "./regrister.js";
 import vitalTaskRouter from "./vitalTask.js";
 import dashboardRouter from "./dashboard.js";
 import categoriesRouter from "./categories.js";
@@ -19,6 +20,7 @@ router.use(settingsRouter);
 router.use(helpRouter);
 */
 
+router.use("/auth/sign-in", registerRouter);
 router.use("/dashboard", dashboardRouter);
 router.use("/tasks", taskRouter);
 router.use("/vital-tasks", vitalTaskRouter);
