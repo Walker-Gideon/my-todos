16fe59e8384eb26b211d29f791e4f64a3753a5f5
Walker-Gideon <gideonawortwe3@gmail.com>
2026-04-08 03:15:02 +0000

    Fix bug in the todoControllers file

--- backend/src/controllers/toDoControllers.js
+++ backend/src/controllers/toDoControllers.js
@@ -5,13 +5,13 @@ import todoModel from "../models/toDoModel.js";
  * @route POST /api/task && /api/vitalTask
  * @access Private
  */
-const createTodo = async (req, res) => {
+export const createTodo = async (req, res) => {
     try {
         const { userId, title, description, priority, image, completed } = req.body;
         const todo = await todoModel.create({ userId, title, description, priority, image, completed });
         res.status(201).json(todo);
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        res.status(500).json({ message: "Failed to create todo" });
     }
 }
 
@@ -20,7 +20,11 @@ const createTodo = async (req, res) => {
  * @route GET /api/dashboard, /api/task, /api/vitalTask
  * @access Private
  */
-const getTodo = async (req, res) => {
-    const todos = await todoModel.find();
-    res.send(todos);
+export const getTodo = async (req, res) => {
+    try {
+        const todos = await todoModel.find();
+        res.status(200).json(todos);
+    } catch (error) {
+        res.status(500).json({ message: "Failed to get todos" });
+    }
 }
