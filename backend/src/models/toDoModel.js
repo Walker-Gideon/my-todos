8abf0bf45c8cd6823c54218d1928a14c0ca4009a
Walker-Gideon <gideonawortwe3@gmail.com>
2026-04-09 02:36:02 +0000

    Update the todoModel with the isVital schema

--- backend/src/models/toDoModel.js
+++ backend/src/models/toDoModel.js
@@ -26,6 +26,10 @@ const todoSchema = new mongoose.Schema({
     completed: {
         type: Boolean,
         default: false
+    },
+    isVital: {
+        type: Boolean,
+        default: false
     }
 }, { timestamps: true })
 
