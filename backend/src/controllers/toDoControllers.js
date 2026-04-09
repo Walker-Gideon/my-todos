e1a740483c904a55f9344deb24e6ca99179e35dd
Walker-Gideon <gideonawortwe3@gmail.com>
2026-04-09 02:34:48 +0000

    Update the todo controllers with the get tast and vital task logic

--- backend/src/controllers/toDoControllers.js
+++ backend/src/controllers/toDoControllers.js
@@ -7,11 +7,10 @@ import todoModel from "../models/toDoModel.js";
  */
 export const createTodo = async (req, res) => {
     try {
-        const { userId, title, description, priority, image, completed } = req.body;
-        const todo = await todoModel.create({ userId, title, description, priority, image, completed });
+        const { userId, title, description, priority, image, completed, isVital } = req.body;
+        const todo = await todoModel.create({ userId, title, description, priority, image, completed, isVital });
         res.status(201).json(todo);
     } catch (error) {
-        console.error("Create Todo Error:", error);
         res.status(500).json({ message: "Failed to create todo", error: error.message });
     }
 }
@@ -27,5 +26,33 @@ export const getTodo = async (req, res) => {
         res.status(200).json(todos);
     } catch (error) {
         res.status(500).json({ message: "Failed to get todos" });
+    }
+}
+
+/**
+ * @desc Get task todos
+ * @route GET /api/task
+ * @access Private
+ */
+export const getTaskTodo = async (req, res) => {
+    try {
+        const todos = await todoModel.find({ isVital: false });
+        res.status(200).json(todos);
+    } catch (error) {
+        res.status(500).json({ message: "Failed to get task todos" });
+    }
+}
+
+/**
+ * @desc Get vital todos
+ * @route GET /api/vitalTask
+ * @access Private
+ */
+export const getVitalTodo = async (req, res) => {
+    try {
+        const todos = await todoModel.find({ isVital: true });
+        res.status(200).json(todos);
+    } catch (error) {
+        res.status(500).json({ message: "Failed to get vital todos" });
     }
 }
