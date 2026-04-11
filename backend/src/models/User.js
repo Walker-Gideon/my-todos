4df84a22ddd29d8a4bae8a59a878f9ec58423edf
Walker-Gideon <gideonawortwe3@gmail.com>
2026-04-11 04:26:10 +0000

    Update the User model with the auth middleware

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,3 +1,4 @@
+import bcrypt from "bcryptjs";
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
@@ -24,5 +25,18 @@ const userSchema = new mongoose.Schema({
         required: true
     }
 }, { timestamps: true });
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
 
 export default mongoose.model("User", userSchema);
