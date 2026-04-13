41776563cfe4c85fee856724cd1b2700c4a7e141
Walker-Gideon <gideonawortwe3@gmail.com>
2026-04-13 16:56:29 +0000

    Set the route and update in each route path

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,14 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
-import Login from "./components/pages/auth/login/Login";
-import AuthLayout from "./components/pages/auth/AuthLayout";
-import Register from "./components/pages/auth/register/Register";
+import AppLayout from "./components/pages";
+import Task from "./components/pages/task";
+import VitalTask from "./components/pages/vital";
+import AuthLayout from "./components/pages/auth";
+import Login from "./components/pages/auth/login";
+import Settings from "./components/pages/settings";
+import Dashboard from "./components/pages/dashboard";
+import Categories from "./components/pages/categories";
+import Register from "./components/pages/auth/register";
 
 export default function App() {
   return (
@@ -16,13 +22,20 @@ export default function App() {
           <Route path="auth/login" element={<Login />} />
         </Route>
 
-        {/* <Route
-            element={
-              <ProtectedRoute>
-                <AppLayout />
-              </ProtectedRoute>
-            }
-          ></Route> */}
+        <Route
+          element={
+            // <ProtectedRoute>
+              <AppLayout />
+            // </ProtectedRoute>
+          }
+        >
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="task" element={<Task />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="vital-task" element={<VitalTask />} />
+          <Route path="categories" element={<Categories />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
--- frontend/src/components/pages/vital/VatialTask.tsx
+++
@@ -1,5 +0,0 @@
-export default function VatialTask() {
-    return (
-        <p>VatialTask</p>
-    )
-}
