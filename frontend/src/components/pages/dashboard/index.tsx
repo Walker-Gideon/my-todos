bdd4e218819b36a4d93acd2627114013b771ffba
Walker-Gideon <gideonawortwe3@gmail.com>
2026-04-14 02:03:14 +0000

    Set the Layout for the Dashboard section

--- frontend/src/components/pages/dashboard/index.tsx
+++ frontend/src/components/pages/dashboard/index.tsx
@@ -1,5 +1,20 @@
+import Container from "@/components/layout/Container";
+
 export default function Dashboard() {
     return (
-        <p>Dashboard</p>
+        <Container container="div" className={""}>
+            <Container variant="header" className={"w-full h-full bg-primary mb-4"}>
+                <p>Header</p>
+            </Container>
+
+            <Container container="div" className={"w-full h-full flex gap-4"}>
+                <Container variant="aside" className={"w-1/5 h-full bg-primary"}>
+                    <p>Aside</p>
+                </Container>
+                <Container variant="main" className={"w-4/5 h-full bg-green-500"}>
+                    <p>Main</p>
+                </Container>
+            </Container>
+        </Container>
     )
 }
