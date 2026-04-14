972848743d0b88153596166fe545553b4110c7e3
Walker-Gideon <gideonawortwe3@gmail.com>
2026-04-14 02:24:18 +0000

    Update in the dashboard and adjustment in the Header

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -11,8 +11,15 @@ export default function Header() {
     */
 
     return (
-        <Container variant="header" className={"w-full h-full bg-primary mb-4"}>
-            <p>Header</p>
+        <Container variant="header" className={"flex gap-4 w-full h-full bg-primary mb-4"}>
+            <p>Navigation</p>
+
+            <Container variant="div" className={"flex items-center justify-between w-full h-full bg-primary gap-4"}>
+                <p>Search bar</p>
+                <p>Notifications</p>
+                <p>Calendar</p>
+                <p>Date</p>
+            </Container>
         </Container>
     )
 }
--- frontend/src/components/pages/dashboard/index.tsx
+++ frontend/src/components/pages/dashboard/index.tsx
@@ -1,19 +1,16 @@
+import Header from "@/components/layout/Header";
+import DashboardMain from "./components/DashboardMain";
 import Container from "@/components/layout/Container";
+import Navigation from "@/components/layout/Navigation";
 
 export default function Dashboard() {
     return (
         <Container container="div" className={""}>
-            <Container variant="header" className={"w-full h-full bg-primary mb-4"}>
-                <p>Header</p>
-            </Container>
+            <Header />
 
             <Container container="div" className={"w-full h-full flex gap-4"}>
-                <Container variant="aside" className={"w-1/5 h-full bg-primary"}>
-                    <p>Aside</p>
-                </Container>
-                <Container variant="main" className={"w-4/5 h-full bg-green-500"}>
-                    <p>Main</p>
-                </Container>
+                <Navigation />
+                <DashboardMain />
             </Container>
         </Container>
     )
