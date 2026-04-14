7213fa4c264155c04b62cede30d7a259f4cd3e37
Walker-Gideon <gideonawortwe3@gmail.com>
2026-04-14 03:19:32 +0000

    Woking on the Search input

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,24 +1,47 @@
-import Container from "./Container";
+import { TbBell, TbSearch, TbCalendarWeek } from "react-icons/tb";
+
+import Input from "@/components/ui/Input";
+import Headings from "@/components/ui/Headings";
+import Container from "@/components/layout/Container";
 
 export default function Header() {
     /*
     List of elements in the header:
-    - Navigation
+    - Navigation (Title)
     - Search bar
     - Notifications
     - Calendar
     - Date
     */
 
-    return (
-        <Container variant="header" className={"flex gap-4 w-full h-full bg-primary mb-4"}>
-            <p>Navigation</p>
+    const styling = {
+        inputContainer: "w-full flex items-center border border-white rounded-md text-sm hover:border-primary focus-within:border-primary focus-within:ring-1 focus-within:ring-primary/20 shadow-lg shadow-primary/50 transition-all",
+        input: "w-full focus:outline-none p-2 bg-transparent",
+        icon: "text-col-white bg-primary h-9 w-9 p-2 rounded-md"
+    }
 
-            <Container variant="div" className={"flex items-center justify-between w-full h-full bg-primary gap-4"}>
-                <p>Search bar</p>
-                <p>Notifications</p>
-                <p>Calendar</p>
-                <p>Date</p>
+    return (
+        <Container variant="header" className={"flex gap-4 w-full h-full bg-col-white-2 mb-6 h-15 p-4 shadow-md"}>
+            <Headings variant="h1">Navigation</Headings>
+
+            <Container variant="div" className={"flex items-center justify-between w-full h-full gap-4"}>
+                <form className={styling["inputContainer"]}>
+                    <Input 
+                        type="text" 
+                        defaultStyling={false} 
+                        placeholder="Search your task here..." 
+                        className={styling["input"]} 
+                    />
+                    <button type="submit">
+                        <TbSearch size={30} className={styling["icon"]} />
+                    </button>
+                </form>
+
+                <Container variant="div" className={"flex items-center gap-4"}>
+                    <p>Notifications</p>
+                    <p>Calendar</p>
+                    <p>Date</p>
+                </Container>
             </Container>
         </Container>
     )
