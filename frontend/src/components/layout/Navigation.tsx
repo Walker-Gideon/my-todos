a9bdcb65a6bed482917d85ce1f17f22ded5d8ab8
Walker-Gideon <gideonawortwe3@gmail.com>
2026-04-16 18:40:28 +0000

    Fix bug in Ui for navigation component

--- frontend/src/components/layout/Navigation.tsx
+++ frontend/src/components/layout/Navigation.tsx
@@ -2,8 +2,8 @@ import { motion } from "motion/react";
 import { NavLink } from "react-router-dom";
 import type { IconType } from "react-icons";
 
-import { TbLogout } from "react-icons/tb";
 import { navItems } from "@/components/data/navLinks";
+import { TbLogout, TbLayoutSidebar } from "react-icons/tb";
 
 import Container from "./Container";
 import Span from "@/components/ui/Span";
@@ -25,7 +25,8 @@ export default function Navigation({ show, onClick }: { show?: boolean, onClick?
                 exit: { x: "-100%" },
                 transition: { type: "spring", damping: 25, stiffness: 200 }
             } : {})}
-            className={`fixed md:relative flex flex-col w-72 md:w-18 midmax:w-64 h-screen md:h-full bg-primary text-col-white p-4 md:pt-12 shadow-xl border-r border-r-primary rounded-r-md md:mt-4 transition-all duration-300 ease-in-out ${show ? "z-50" : "hidden md:flex"}`}
+            // md:w-18 midmax:w-64
+            className={`fixed md:relative flex flex-col w-72 h-screen md:h-full bg-primary text-col-white p-4 md:pt-12 shadow-xl border-r border-r-primary rounded-r-md md:mt-4 transition-all duration-300 ease-in-out ${show ? "z-50" : "hidden md:flex"}`}
             onClick={(e) => e.stopPropagation()}
         >
             <Profile />
@@ -36,9 +37,11 @@ export default function Navigation({ show, onClick }: { show?: boolean, onClick?
                     ))}
                 </Container>
                 <Container variant="div" className={"mt-auto md:mb-2 pt-2 border-t border-white/10 overflow-hidden"}>
-                    <Button onClick={handleLogout} className={"w-full flex items-center justify-center midmax:justify-start gap-3 hover:bg-white/20 transition-all px-2 py-2.5 rounded-md text-sm"}>
+                {/* justify-center midmax:justify-start */}
+                    <Button onClick={handleLogout} className={"w-full flex items-center justify-start gap-3 hover:bg-white/20 transition-all px-2 py-2.5 rounded-md text-sm"}>
                         <TbLogout size={20} className={"shrink-0"} />
-                        <Span className={"font-medium hidden midmax:block"}>Logout</Span>
+                        {/* hidden midmax:block */}
+                        <Span className={"font-medium"}>Logout</Span>
                     </Button>
                 </Container>
             </Container>
@@ -48,19 +51,27 @@ export default function Navigation({ show, onClick }: { show?: boolean, onClick?
 
 function Profile() {
     return (
-        <Container variant="div" className={"flex flex-row md:flex-col gap-4 md:gap-0 items-center mb-4 pb-4 border-b border-white/10"}>
-            <Container variant="div" className={"md:absolute md:-top-8 md:left-1/2 md:-translate-x-1/2"}>
-                <img 
-                    src="https://ui-avatars.com/api/?name=User&background=212427&color=fff" 
-                    alt="user image" 
-                    className={"w-15 h-15 md:w-15 midmax:w-20 md:h-15 midmax:h-20 rounded-full border-2 border-col-white-2 shadow-lg object-cover bg-dark"} 
-                />
+        <Container variant="div" className={"mb-4 pb-4 border-b border-white/10"}>
+            {/* flex flex-row  */}
+            <Container variant="div" className={"flex flex-row md:flex-col items-center gap-4 md:gap-0"}>
+                <Container variant="div" className={"md:absolute md:-top-8 md:left-1/2 md:-translate-x-1/2"}>
+                    <img 
+                        src="https://ui-avatars.com/api/?name=User&background=212427&color=fff" 
+                        alt="user image" 
+                        className={"w-15 h-15 md:w-15 midmax:w-20 md:h-15 midmax:h-20 rounded-full border-2 border-col-white-2 shadow-lg object-cover bg-dark"} 
+                    />
+                </Container>
+
+                {/* hidden md:block */}
+                <Container variant="div" className={"text-center mt-1 text-white"}>
+                    <Paragraph className={"text-lg md:text-base font-semibold"}>Full Name</Paragraph>
+                    <Paragraph variant="small" className={"leading-none"}>user@email.com</Paragraph>
+                </Container>
             </Container>
 
-            <Container variant="div" className={"hidden md:block text-center mt-1 text-white"}>
-                <Paragraph className={"font-semibold"}>Full Name</Paragraph>
-                <Paragraph variant="small" className={"leading-none"}>user@email.com</Paragraph>
-            </Container>
+            <Button className={"hidden md:block"}>
+                <TbLayoutSidebar size={20} />
+            </Button>
         </Container>
     )
 }
@@ -71,8 +82,9 @@ function NavItems({ name, icon: Icon, href, onClick }: { name: string, icon: Ico
             to={href} 
             end
             onClick={onClick}   
+            // justify-center midmax:justify-start
             className={({ isActive }) =>
-              `flex items-center justify-center midmax:justify-start gap-3 p-2 rounded-md transition-all duration-300 group ${
+              `flex items-center justify-start gap-3 p-2 rounded-md transition-all duration-300 group ${
                 isActive 
                   ? "bg-white text-primary shadow-lg shadow-white/10" 
                   : "text-white hover:text-white hover:bg-white/5"
@@ -80,7 +92,8 @@ function NavItems({ name, icon: Icon, href, onClick }: { name: string, icon: Ico
             }
         >
             <Icon size={20} className={"shrink-0"} />
-            <Span className={"font-medium hidden midmax:block truncate"}>{name}</Span>
+            {/* hidden midmax:block truncate */}
+            <Span className={"font-medium"}>{name}</Span>
         </NavLink>
     )
 }
