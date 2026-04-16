aba0bcf7a29f5f3a338da4e793e949f041cdf039
Walker-Gideon <gideonawortwe3@gmail.com>
2026-04-16 04:31:16 +0000

    Update in the Dashboard

--- frontend/src/components/pages/dashboard/index.tsx
+++ frontend/src/components/pages/dashboard/index.tsx
@@ -13,7 +13,7 @@ export default function Dashboard() {
 
    // <DashboardMain />
     return (
-        <Container variant="main" className={"w-full h-full"}>
+        <Container variant="main" className={"w-full h-full flex flex-col"}>
             <Container variant="header" className={"w-full flex gap-2 md:gap-0 flex-col md:flex-row items-center md:justify-between mb-2"}>
                 <Container variant="div" className={"w-full md:flex-1 flex min-w-0 items-center flex-row gap-2 text-3xl"}>
                     <Paragraph className={"font-medium truncate"}>
@@ -28,7 +28,7 @@ export default function Dashboard() {
                 </Container>
             </Container>
 
-            <Container variant="div" className={"w-full h-full border border-gray-300"}>
+            <Container variant="div" className={"w-full flex-1 border border-gray-300"}>
                 <Container variant="div" className={"w-full"}>
                     <p>Todos</p>
                 </Container>
