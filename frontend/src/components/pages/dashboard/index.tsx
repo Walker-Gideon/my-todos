8951425e735942244884fb3f6b2a0a8a74e1ac2e
Walker-Gideon <gideonawortwe3@gmail.com>
2026-04-17 15:08:50 +0000

    Work on the ToDo display header

--- frontend/src/components/pages/dashboard/index.tsx
+++ frontend/src/components/pages/dashboard/index.tsx
@@ -1,7 +1,7 @@
 import Container from "@/components/layout/Container";
 import ShadowBox from "@/components/layout/ShadowBox";
-import DashboardHeader from "./components/DashboardHeader";
 import DashboardTodos from "./components/DashboardTodos";
+import DashboardHeader from "./components/DashboardHeader";
 
 export default function Dashboard() {
     /*
@@ -15,15 +15,15 @@ export default function Dashboard() {
         <Container variant="main" className={"w-full h-full flex flex-col"}>
             <DashboardHeader />
             <Container variant="div" className={"w-full flex flex-col md:flex-row flex-1 gap-4 p-4 primary-border"}>
-                <ShadowBox>
+                <ShadowBox className={"px-6"}>
                     <DashboardTodos />
                 </ShadowBox>
 
                 <Container variant="div" className={"w-full flex flex-col gap-4"}>
-                    <ShadowBox>
+                    <ShadowBox className={"px-6"}>
                         <p>Task Status</p>
                     </ShadowBox>
-                    <ShadowBox>
+                    <ShadowBox className={"px-6"}>
                         <p>Completed Task</p>
                     </ShadowBox>
                 </Container>
