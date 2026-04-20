43d18be90e0123a9a2ba289c249d5821ecfe30ab
Walker-Gideon <gideonawortwe3@gmail.com>
2026-04-20 05:04:27 +0000

    Update the Input component with disabled styling

--- frontend/src/components/ui/Input.tsx
+++ frontend/src/components/ui/Input.tsx
@@ -2,8 +2,9 @@ import type { InputProps } from "./interfaces";
 
 export default function Input({ type, placeholder, className, defaultStyling, disabled, ...rest }: InputProps) {
     const defaultClasses = defaultStyling ? "border border-border-primary rounded-md px-3 py-2 text-sm" : "";
+    const disabledStyling = "disabled:opacity-50 disabled:cursor-not-allowed focus:outline-none";
 
     return (
-        <input disabled={disabled} type={type} placeholder={placeholder} className={`${defaultClasses} ${className}`} {...rest} />
+        <input disabled={disabled} type={type} placeholder={placeholder} className={`${defaultClasses} ${className} ${disabledStyling}`} {...rest} />
     )
 }
