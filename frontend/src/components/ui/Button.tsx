8e5d3137a05a0773b7b37f5a1dd1f4551af4dd4c
Walker-Gideon <gideonawortwe3@gmail.com>
2026-04-20 04:10:11 +0000

    Adjustment in the Button UI component

--- frontend/src/components/ui/Button.tsx
+++ frontend/src/components/ui/Button.tsx
@@ -1,11 +1,18 @@
 import type { ButtonProps } from "./interfaces";
 
-export default function Button({ children, ariaLabel, type, className, variant, ...rest }: ButtonProps) {
-    const defaultStyling = "cursor-pointer text-sm";
+export default function Button({ children, ariaLabel, type, className, disabled, variant, ...rest }: ButtonProps) {
+    const defaultStyling = "text-sm transition-all duration-300 ease-in-out";
+    const disabledStyling = "disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-primary disabled:hover:text-col-white";
     
     if (variant === "primary") {
         return (
-            <button aria-label={ariaLabel} type={type} className={`${defaultStyling} bg-primary hover:bg-primary-hover hover:text-white transition-all duration-300 ease-in-out text-col-white px-4 py-3 rounded-md ${className}`} {...rest}>
+            <button 
+                disabled={disabled} 
+                aria-label={ariaLabel} 
+                type={type} 
+                className={`${defaultStyling} ${disabledStyling} cursor-pointer bg-primary hover:bg-primary-hover hover:text-white text-col-white px-4 py-3 rounded-md ${className}`} 
+                {...rest}
+            >
                 {children}
             </button>
         )
@@ -13,7 +20,13 @@ export default function Button({ children, ariaLabel, type, className, variant, 
 
     if (variant === "outline") {
         return (
-            <button aria-label={ariaLabel} type={type} className={`${defaultStyling} px-4 py-2 rounded-md ${className}`} {...rest}>
+            <button 
+                disabled={disabled} 
+                aria-label={ariaLabel} 
+                type={type} 
+                className={`${defaultStyling} ${disabledStyling} cursor-pointer px-4 py-2 border border-border-primary rounded-md ${className}`} 
+                {...rest}
+            >
                 {children}
             </button>
         )
@@ -21,14 +34,26 @@ export default function Button({ children, ariaLabel, type, className, variant, 
 
     if (variant === "text") {
         return (
-            <button aria-label={ariaLabel} type={type} className={`${defaultStyling} ${className}`} {...rest}>
+            <button 
+                disabled={disabled} 
+                aria-label={ariaLabel} 
+                type={type} 
+                className={`${defaultStyling} ${disabledStyling} cursor-pointer ${className}`} 
+                {...rest}
+            >
                 {children}
             </button>
         )
     }
 
     return (
-        <button aria-label={ariaLabel} type={type} className={`${defaultStyling} ${className}`} {...rest}>
+        <button 
+            disabled={disabled} 
+            aria-label={ariaLabel} 
+            type={type} 
+            className={`${defaultStyling} ${disabledStyling} cursor-pointer ${className}`} 
+            {...rest}
+        >
             {children}
         </button>
     )
