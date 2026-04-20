fdcc1cbbfa7aeeea4d5e9805ae2ee3f764eeb76b
Walker-Gideon <gideonawortwe3@gmail.com>
2026-04-20 04:10:55 +0000

    Adjustment in the Input UI component

--- frontend/src/components/ui/Input.tsx
+++ frontend/src/components/ui/Input.tsx
@@ -1,9 +1,9 @@
 import type { InputProps } from "./interfaces";
 
-export default function Input({ type, placeholder, className, defaultStyling, ...rest }: InputProps) {
+export default function Input({ type, placeholder, className, defaultStyling, disabled, ...rest }: InputProps) {
     const defaultClasses = defaultStyling ? "border border-border-primary rounded-md px-3 py-2 text-sm" : "";
 
     return (
-        <input type={type} placeholder={placeholder} className={`${defaultClasses} ${className}`} {...rest} />
+        <input disabled={disabled} type={type} placeholder={placeholder} className={`${defaultClasses} ${className}`} {...rest} />
     )
 }
