aa7d14ef2d6ecff0a3e145b82bfe969b19c9bfdb
Walker-Gideon <gideonawortwe3@gmail.com>
2026-04-20 04:08:17 +0000

    Adjustment to the loginForm component

--- frontend/src/components/pages/auth/login/components/LoginForm.tsx
+++ frontend/src/components/pages/auth/login/components/LoginForm.tsx
@@ -1,37 +1,33 @@
 import { useNavigate } from "react-router-dom";
-import { useForm, type SubmitHandler } from "react-hook-form"
 import { TbLockFilled, TbUserFilled } from "react-icons/tb";
+import { useForm, type SubmitHandler } from "react-hook-form"
 
 import Span from "@/components/ui/Span";
 import Label from "@/components/ui/Label";
 import Input from "@/components/ui/Input";
 import Button from "@/components/ui/Button";
+import Toast from "@/components/layout/Toast";
 import Paragraph from "@/components/ui/Paragraph";
 import Container from "@/components/layout/Container";
+import Conditional from "@/components/layout/Conditional";
 
 import { useLoginUser } from "@/components/pages/auth/hooks/useLoginUser";
 
 type LoginData = {
   email: string
   password: string
+  rememberMe: boolean
 }
 
 export default function LoginForm() {
     const navigate = useNavigate();
-    const { login, isPending, error } = useLoginUser();
+    const { register, handleSubmit } = useForm<LoginData>()
+    const { login, isPending, error, reset: resetMutation, isSuccess } = useLoginUser();
 
-    const { 
-        register,
-        handleSubmit,
-        reset,
-    } = useForm<LoginData>()
-
-    const onSubmit: SubmitHandler<LoginData> = (data) => {
+    const onSubmit: SubmitHandler<LoginData> = async (data) => {
+        await new Promise(resolve => setTimeout(resolve, 800));
         login(data);
     }
-
-    console.log(isPending)
-    console.log(error)
 
     const styling = {
         inputContainer: "flex items-center border border-border-primary rounded-md pl-3 text-sm hover:border-primary-hover focus-within:border-primary focus-within:ring-1 focus-within:ring-primary/20 transition-all",
@@ -46,9 +42,16 @@ export default function LoginForm() {
                 <Input 
                     type="text" 
                     defaultStyling={false} 
-                    placeholder="Username" 
+                    placeholder="Email" 
                     className={styling["input"]} 
-                    {...register("email")}
+                    {...register("email", { 
+                        required: "Email is required",
+                        disabled: isPending,
+                        pattern: {
+                            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                            message: "Invalid email address"
+                        }
+                    })}
                 />
             </Container>
 
@@ -59,7 +62,14 @@ export default function LoginForm() {
                     defaultStyling={false} 
                     placeholder="Password" 
                     className={styling["input"]} 
-                    {...register("password")}
+                    {...register("password", { 
+                        required: "Password is required",
+                        disabled: isPending,
+                        minLength: {
+                            value: 8,
+                            message: "Password must be at least 8 characters"
+                        }
+                    })}
                 />
             </Container>
 
@@ -67,8 +77,10 @@ export default function LoginForm() {
                 <Label className={"flex items-center gap-2 cursor-pointer group"}>
                     <Input 
                         type="checkbox" 
+                        disabled={isPending}
                         defaultStyling={false} 
-                        className={"w-4 h-4 cursor-pointer accent-primary"} 
+                        className={"w-4 h-4 cursor-pointer accent-primary disabled:cursor-not-allowed"} 
+                        {...register("rememberMe")}
                     />
                     <Span className={"text-sm text-zinc-600 dark:text-zinc-400"}>Remember Me</Span>
                 </Label>
@@ -77,9 +89,10 @@ export default function LoginForm() {
                     type="submit"
                     ariaLabel="Sign In"
                     variant="primary"
-                    className={"w-full font-semibold shadow-lg"}
+                    disabled={isPending}
+                    className={"w-full font-semibold shadow-lg py-3"}
                 >
-                    Sign In
+                    {isPending ? "Signing In..." : "Sign In"}
                 </Button>
 
                 <div className={"flex items-center justify-center gap-2 text-sm text-zinc-500"}>
@@ -87,13 +100,34 @@ export default function LoginForm() {
                     <Button
                         variant="text"
                         ariaLabel="Navigate to register page"
+                        disabled={isPending}
                         onClick={(e) => { e.preventDefault(); navigate("/auth/register") }}
-                        className={"text-primary font-semibold hover:underline transition-all"}
+                        className={`text-primary font-semibold hover:underline transition-all ${isPending ? "cursor-not-allowed" : "cursor-pointer"}`}
                     >
                         Create One
                     </Button>
                 </div>
             </div>
+            <Conditional condition={isSuccess}>
+                <Toast
+                    message={"Login successfully"}
+                    type="success"
+                    isVisible={isSuccess}
+                    onClose={() => {
+                        resetMutation();
+                    }}
+                />
+            </Conditional>
+            <Conditional condition={!!error}>
+                <Toast
+                    message={error?.message || ""}
+                    type="error"
+                    isVisible={!!error}
+                    onClose={() => {
+                        resetMutation();
+                    }}
+                />
+            </Conditional>
         </form>
     )
 }
