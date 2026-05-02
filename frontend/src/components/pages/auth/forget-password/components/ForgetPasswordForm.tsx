239a12d9dc64d95d25624cbc5c38eb476920d1d8
Walker-Gideon <gideonawortwe3@gmail.com>
2026-05-02 21:11:10 +0000

    Update the Forget password with the api logic

--- frontend/src/components/pages/auth/forget-password/components/ForgetPasswordForm.tsx
+++ frontend/src/components/pages/auth/forget-password/components/ForgetPasswordForm.tsx
@@ -6,24 +6,28 @@ import Input from "@/components/ui/Input";
 import Button from "@/components/ui/Button";
 import FormRow from "@/components/pages/auth/components/FormRow";
 
+import { useForgetPassword } from "@/components/pages/auth/hooks/useForgetPassword";
+
 type ForgetPasswordInput = {
   email: string
 }
 
 export default function ForgetPasswordForm() {
+    const { forget, isPending } = useForgetPassword();
     const { register, handleSubmit, formState: { errors } } = useForm<ForgetPasswordInput>();
 
     const onSubmit: SubmitHandler<ForgetPasswordInput> = async (data) => {
         await new Promise(resolve => setTimeout(resolve, 800));
-        console.log(data);
+        forget(data);
     }
-    
+
     return (
         <form onSubmit={handleSubmit(onSubmit)} className={"flex flex-col gap-5 w-full"}>
             <FormRow errorsField={errors.email} errorMessage={errors.email?.message}>
                 <TbUserFilled className={"text-zinc-400"} />
                 <Input 
                     type="email" 
+                    disabled={isPending}
                     defaultStyling={false} 
                     placeholder="Email" 
                     className={"w-full focus:outline-none px-2 py-2.5 md:p-2 bg-transparent"}
@@ -40,9 +44,10 @@ export default function ForgetPasswordForm() {
                 type="submit"
                 ariaLabel="Send Reset Link"
                 variant="primary"
+                disabled={isPending}
                 className={"w-full font-semibold shadow-lg py-3"}
             >
-                Send Reset Link
+                {isPending ? "Sending..." : "Send Reset Link"}
             </Button>
         </form>
     )
