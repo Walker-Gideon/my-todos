0eb6f87a5c9b99bbf36d86f563a9d47732346d6c
Walker-Gideon <gideonawortwe3@gmail.com>
2026-05-03 23:39:22 +0000

    Add the login logic to the register hook

--- frontend/src/components/pages/auth/hooks/useLoginUser.ts
+++ frontend/src/components/pages/auth/hooks/useLoginUser.ts
@@ -3,14 +3,17 @@ import { useNavigate } from 'react-router-dom';
 import { useMutation } from '@tanstack/react-query';
 
 import { loginUser, type LoginData } from '@/api/auth';
+import { useAuth } from '@/context/AuthContext';
 
 export const useLoginUser = () => {
     const navigate = useNavigate();
+    const { login: updateGlobalUser } = useAuth();
 
     const { mutate: login, isPending } = useMutation({
         mutationFn: (data: LoginData) => loginUser(data),
         onSuccess: (data) => {
             localStorage.setItem('token', data.token);
+            updateGlobalUser(data);
             navigate('/dashboard');
             toast.success("Login successfully");
         },
