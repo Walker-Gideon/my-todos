e01435fad6c72f2fa422e04ccf94aecf96adcb09
Walker-Gideon <gideonawortwe3@gmail.com>
2026-05-03 23:42:05 +0000

    Update the public route base on the auth context

--- frontend/src/components/utils/PublicRoute.tsx
+++ frontend/src/components/utils/PublicRoute.tsx
@@ -1,9 +1,18 @@
+import { useAuth } from "@/context/AuthContext";
 import { Navigate, Outlet } from "react-router-dom";
 
 export default function PublicRoute() {
-    const token = localStorage.getItem("token");
+    const { isAuthenticated, isLoading } = useAuth();
 
-    if (token) {
+    if (isLoading) {
+        return (
+            <div className="flex items-center justify-center h-screen bg-slate-50">
+                <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-primary"></div>
+            </div>
+        );
+    }
+
+    if (isAuthenticated) {
         return <Navigate to="/dashboard" replace />;
     }
 
