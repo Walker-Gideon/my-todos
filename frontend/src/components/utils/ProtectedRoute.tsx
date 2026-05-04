8040df497861d1016d2a1ac0228dddc0d951389e
Walker-Gideon <gideonawortwe3@gmail.com>
2026-05-04 20:11:23 +0000

    Update the ProtectedRoute with the Spinner

--- frontend/src/components/utils/ProtectedRoute.tsx
+++ frontend/src/components/utils/ProtectedRoute.tsx
@@ -1,13 +1,15 @@
 import { useAuth } from "@/context/AuthContext";
 import { Navigate, Outlet } from "react-router-dom";
 
+import Spinner from "@/components/layout/Spinner";
+
 export default function ProtectedRoute() {
     const { isAuthenticated, isLoading } = useAuth();
 
     if (isLoading) {
         return (
             <div className="flex items-center justify-center h-screen bg-slate-50">
-                <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-primary"></div>
+                <Spinner size="h-12 w-12" color="border-primary" />
             </div>
         );
     }
