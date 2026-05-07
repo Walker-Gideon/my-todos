bbdf7cf790518616af0a09d258d8e8e23c9e5745
Walker-Gideon <gideonawortwe3@gmail.com>
2026-05-07 03:28:27 +0000

    Update in the Vital component

--- frontend/src/components/pages/vital/index.tsx
+++ frontend/src/components/pages/vital/index.tsx
@@ -1,5 +1,17 @@
+import Container from "@/components/layout/Container";
+
 export default function VitalTask() {
     return (
-        <p>VitalTask</p>
+        <Container 
+            variant="main" 
+            className={"w-full min-h-0 grid grid-cols-2"}
+        >
+            <Container variant="div">
+                <p>Vital Task</p>
+            </Container>
+            <Container variant="div">
+                <p>Vital Task</p>
+            </Container>
+        </Container>
     )
 }
