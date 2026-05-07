3504b82ab5b7cac6d301d3b74a7a21e0c747050d
Walker-Gideon <gideonawortwe3@gmail.com>
2026-05-07 03:26:57 +0000

    Update in the Dashboard Todo component

--- frontend/src/components/pages/dashboard/components/DashboardTodos.tsx
+++ frontend/src/components/pages/dashboard/components/DashboardTodos.tsx
@@ -12,19 +12,23 @@ import { useDateFormat } from "@/components/hooks/useDateFormat";
 
 export default function DashboardTodos() {
     const { day, monthInWords } = useDateFormat();
-    /*
-    List of elements in the main:
-    - Header (Todo icon, date)
-    - Main content (Todos)
-    */
    
     function handleAddTodo() {}
 
     return (
-        <ShadowBox className={"px-4 md:px-6 flex flex-col max-h-none md:max-h-[400px]"}>
-            <Container variant="header" className={"w-full flex flex-col gap-2"}>
-                <Container variant="div" className={"w-full flex flex-row items-center justify-between"}>
-                    <SubHeading icon={<TbClipboard size={24} />} subheading={"To-Do"} />
+        <ShadowBox className={"px-4 md:px-6 flex flex-col min-h-0 max-h-none md:max-h-[600px]"}>
+            <Container 
+                variant="header" 
+                className={"w-full flex flex-col gap-2"}
+            >
+                <Container 
+                    variant="div" 
+                    className={"w-full flex flex-row items-center justify-between"}
+                >
+                    <SubHeading 
+                        icon={<TbClipboard size={24} />} 
+                        subheading={"To-Do"} 
+                    />
                     <Button
                         variant="text"
                         ariaLabel="Add task"
@@ -35,7 +39,10 @@ export default function DashboardTodos() {
                         <Span>Add task</Span>
                     </Button>
                 </Container>
-                <Container variant="div" className="w-full flex flex-col gap-4">
+                <Container 
+                    variant="div" 
+                    className={"w-full flex flex-col gap-4"}
+                >
                     <Paragraph className={"text-dark flex items-center gap-2 text-sm"}>
                         {day}{", "}{monthInWords} 
                         <Span className={"text-gray flex items-center justify-center gap-1"}>
@@ -52,7 +59,11 @@ export default function DashboardTodos() {
 
 function DashboardTodosList() {
     return (
-        <Container variant="main" className="w-full mt-4 flex-none md:flex-1 flex flex-col gap-4 overflow-visible md:overflow-y-auto min-h-0">
+        <Container 
+            variant="main" 
+            // w-full mt-4 flex-1 min-h-0 flex flex-col gap-4 overflow-y-auto
+            className={"w-full mt-4 flex-none md:flex-1 flex flex-col gap-4 overflow-visible md:overflow-y-auto"}
+        >
             <Card />
             <Card />
             <Card />
