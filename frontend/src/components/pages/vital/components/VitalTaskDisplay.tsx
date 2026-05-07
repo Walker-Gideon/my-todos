1b3df70444780df88cf28dac4383ce2b34f7ba99
Walker-Gideon <gideonawortwe3@gmail.com>
2026-05-07 21:24:32 +0000

    Update in the vital component

---
+++ frontend/src/components/pages/vital/components/VitalTaskDisplay.tsx
@@ -0,0 +1,23 @@
+import Container from "@/components/layout/Container";
+import ShadowBox from "@/components/layout/ShadowBox";
+import SecondaryHeading from "@/components/layout/SecondaryHeading";
+
+import Card from "@/components/layout/Card";
+
+export default function VitalTaskContent() {
+    return (
+        <ShadowBox className={"w-full mb-2 medium:px-8"}>
+            <SecondaryHeading fristWord="Vital" secondWord="Task" />
+
+            <Container
+                variant="div"
+                className={"mt-4 w-full flex flex-col gap-2"}
+            >
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </Container>
+        </ShadowBox>
+    )
+}
