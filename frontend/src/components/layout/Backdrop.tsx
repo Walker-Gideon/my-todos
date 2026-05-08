7cf8e12447bc604ca8b184fc14edc45ad63bccf3
Walker-Gideon <gideonawortwe3@gmail.com>
2026-05-08 03:33:04 +0000

    Update in the Backdrop component

--- frontend/src/components/layout/Backdrop.tsx
+++ frontend/src/components/layout/Backdrop.tsx
@@ -1,15 +1,18 @@
+import { cn } from "@/lib/utils";
 import { motion } from "motion/react";
 
-export default function Backdrop({ children, onClick, show }: { children: React.ReactNode, onClick: () => void, show: boolean }) {
+export default function Backdrop({ children, onClick, show, center }: { children: React.ReactNode, onClick?: () => void, show: boolean, center?: boolean }) {
     return (
         <motion.div 
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            className={`fixed inset-0 bg-black/50 z-40 ${show ? "block z-40" : "hidden"}`}
+            className={cn(`fixed inset-0 bg-black/50 z-40`, show ? "block z-40" : "hidden", center ? "flex items-center justify-center" : "")}
             onClick={onClick}
         >
-            {children}
+            <div onClick={(e) => e.stopPropagation()}>
+                {children}
+            </div>
         </motion.div>
     )
 }
