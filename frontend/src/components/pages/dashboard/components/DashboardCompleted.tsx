483fef4e54ed4a4c9c7a3e1328ccd059d612b300
Walker-Gideon <gideonawortwe3@gmail.com>
2026-05-12 13:15:01 +0000

    Adjustment in the Dashboard completed abd Todos component

--- frontend/src/components/pages/dashboard/components/DashboardCompleted.tsx
+++ frontend/src/components/pages/dashboard/components/DashboardCompleted.tsx
@@ -4,11 +4,13 @@ import SubHeading from "./SubHeading";
 import Card from "@/components/layout/Card";
 import ShadowBox from "@/components/layout/ShadowBox";
 import Container from "@/components/layout/Container";
+import Conditional from "@/components/layout/Conditional";
+import Information from "@/components/layout/Information";
 
 export default function DashboardCompleted() {
     return (
         <ShadowBox
-            className={"px-4 md:px-6 flex flex-col min-h-0 max-h-[300px] md:max-h-[290px] mb-4 md:mb-0"}
+            className={"px-4 md:px-6 flex flex-col min-h-0 max-h-[500px] md:max-h-none md:flex-1 mb-4 md:mb-0"}
         >
             <SubHeading
                 icon={<TbClipboardCheck size={24} />}
@@ -19,10 +21,15 @@ export default function DashboardCompleted() {
                 variant="main" 
                 className={"w-full mt-4 flex-1 min-h-0 flex flex-col gap-4 overflow-y-auto"}
             >
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+                <Conditional condition={false}>
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                </Conditional>
+                <Conditional condition={true}>
+                    <Information />
+                </Conditional>
             </Container>
         </ShadowBox>
     )
