07655c06954d8445d5ee9abe1491bea61dcc4b44
Walker-Gideon <gideonawortwe3@gmail.com>
2026-05-13 14:30:18 +0000

    Update in the my task index file component

--- frontend/src/components/pages/task/index.tsx
+++ frontend/src/components/pages/task/index.tsx
@@ -1,5 +1,15 @@
+import TaskDisplay from "./components/TaskDisplay";
+import TaskContent from "./components/TaskContent";
+import Container from "@/components/layout/Container";
+
 export default function Task() {
     return (
-        <p>Task</p>
+        <Container 
+            variant="main" 
+            className={"w-full h-full flex flex-col md:flex-row gap-4 min-h-0 overflow-y-auto md:overflow-hidden"}
+        >
+            <TaskDisplay />
+            <TaskContent />
+        </Container>
     )
 }
