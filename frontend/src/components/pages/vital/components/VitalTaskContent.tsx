d4372fa8d9fd42a43ccc8e04d6e083db044ec2bb
Walker-Gideon <gideonawortwe3@gmail.com>
2026-05-13 11:37:57 +0000

    Adjustment in the vital task-content and task-display component

--- frontend/src/components/pages/vital/components/VitalTaskContent.tsx
+++ frontend/src/components/pages/vital/components/VitalTaskContent.tsx
@@ -1,9 +1,8 @@
 import ShadowBox from "@/components/layout/ShadowBox";
 
 export default function VitalTaskContent() {
-    // w-full md:w-3/5 mb-2 hidden md:flex md:bg-blue-200
     return (
-        <ShadowBox className={"px-4 md:px-6 flex flex-col w-full md:w-3/5 min-h-0 max-h-[500px] md:max-h-none md:flex-1 hidden md:flex mb-4 md:mb-0"}>
+        <ShadowBox className={"px-4 md:px-6 flex flex-col w-full md:w-3/5 min-h-0 max-h-[500px] md:max-h-none hidden md:flex mb-4 md:mb-0"}>
             <p>Vital Task display</p>
         </ShadowBox>
     )
--- frontend/src/components/pages/vital/components/VitalTaskDisplay.tsx
+++ frontend/src/components/pages/vital/components/VitalTaskDisplay.tsx
@@ -7,9 +7,8 @@ import SecondaryHeading from "@/components/layout/SecondaryHeading";
 import Card from "@/components/layout/Card";
 
 export default function VitalTaskDisplay() {
-    // md:w-2/5 min-h-0 max-h-[500px] md:max-h-none md:flex-1 md:bg-blue-200
     return (
-        <ShadowBox className={"px-4 md:px-6 flex flex-col w-full md:w-2/5 min-h-0 max-h-[500px] md:max-h-none md:flex-1 mb-4 md:mb-0"}>
+        <ShadowBox className={"px-4 md:px-6 flex flex-col w-full md:w-2/5 min-h-0 max-h-[500px] md:max-h-none mb-4 md:mb-0"}>
             <SecondaryHeading fristWord="Vital" secondWord=" Task" />
 
             <Container 
