b036d56278f5b58dd1f76ed887dc90a5ba49bc1c
Walker-Gideon <gideonawortwe3@gmail.com>
2026-05-13 14:29:43 +0000

    Create and update My-task components

---
+++ frontend/src/components/pages/task/components/TaskContent.jsx
@@ -0,0 +1,9 @@
+import CardContent from "@/components/layout/CardContent";
+
+export default function TaskContent() {
+    return (
+        <>
+            <CardContent className={"hidden md:flex"} />
+        </>
+    )
+}
---
+++ frontend/src/components/pages/task/components/TaskDisplay.tsx
@@ -0,0 +1,31 @@
+import ShadowBox from "@/components/layout/ShadowBox";
+import Container from "@/components/layout/Container";
+import Conditional from "@/components/layout/Conditional";
+import Information from "@/components/layout/Information";
+import SecondaryHeading from "@/components/layout/SecondaryHeading";
+
+import Card from "@/components/layout/Card";
+
+export default function TaskDisplay() {
+    return (
+        <ShadowBox className={"px-4 md:px-6 flex flex-col w-full md:w-2/5 min-h-0 max-h-[500px] md:max-h-none mb-4 md:mb-0"}>
+            <SecondaryHeading fristWord="My" secondWord=" Tasks" />
+
+            <Container 
+                variant="main" 
+                className={"w-full mt-4 flex-1 min-h-0 flex flex-col gap-4 overflow-y-auto"}
+            >
+                <Conditional condition={false}>  
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card /> 
+                </Conditional>
+                <Conditional condition={true}>
+                    <Information />
+                </Conditional>
+            </Container>
+        </ShadowBox>
+    )
+}
