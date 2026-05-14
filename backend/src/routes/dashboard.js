dec4e99d484773cb3a3a16597d6f16c8d113c6a4
Walker-Gideon <gideonawortwe3@gmail.com>
2026-05-14 21:00:03 +0000

    Added the create, edit, delete logic to the following route: dashboard, task, and vital-task and update the index route with the newly created route

--- backend/src/routes/dashboard.js
+++ backend/src/routes/dashboard.js
@@ -1,9 +1,18 @@
 import { Router } from "express";
+import { checkSchema } from "express-validator";
 import { protect } from "../middleware/authMiddleware.js";
-import { getTodo } from "../controllers/todoController.js";
+import { getTodo, getCompletedTodos, createTodo, updateTodo, deleteTodo } from "../controllers/todoController.js";
+import { createTodoValidationSchema } from "../utils/validationSchema.js";
 
 const router = Router();
 
-router.get("/api/dashboard", protect, getTodo)
+// Fetching lists
+router.get("/api/dashboard", protect, getTodo);
+router.get("/api/completed", protect, getCompletedTodos);
+
+// Actions (Create, Update, Delete)
+router.post("/api/task", protect, checkSchema(createTodoValidationSchema), createTodo);
+router.put("/api/task/:id", protect, updateTodo);
+router.delete("/api/task/:id", protect, deleteTodo);
 
 export default router;
--- backend/src/routes/vitalTask.js
+++ backend/src/routes/vitalTask.js
@@ -1,11 +1,10 @@
 import { Router } from "express";
 import { protect } from "../middleware/authMiddleware.js";
-import { resolveIsVital } from "../middleware/middlewares.js";
-import { createTodo, getVitalTodo } from "../controllers/todoController.js";
+import { getVitalTodo } from "../controllers/todoController.js";
 
 const router = Router();
 
-router.post("/api/vitalTask", protect, resolveIsVital, createTodo)
-router.get("/api/vitalTask", protect, getVitalTodo)
+// This route only handles fetching the Vital Task list
+router.get("/api/vitalTask", protect, getVitalTodo);
 
 export default router;
