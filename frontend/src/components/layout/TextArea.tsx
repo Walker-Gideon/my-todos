95b2198d881dae0e6c9603dde4eb5e62cbef27ec
Walker-Gideon <gideonawortwe3@gmail.com>
2026-05-16 19:25:55 +0000

    Code refactoring in the TestArea component

--- frontend/src/components/layout/TextArea.tsx
+++ frontend/src/components/layout/TextArea.tsx
@@ -1,37 +1,36 @@
+import { forwardRef } from "react";
 import { cn } from "@/lib/utils";
+
 import ErrorMessage from "./ErrorMessage";
 
 import type { TextAreaProps } from "./interfaces";
 
-export default function TextArea({ id, label, name, value, onChange, className, error, message, placeholder, rows, maxLength, disabled }: TextAreaProps) {
-    return (
-        <div className="flex flex-col gap-1 w-full">
-            <label 
-                htmlFor={id} 
-                className={cn(`text-sm text-dark font-semibold`, className)}
-            >
-                {label}
-            </label>
-            <textarea 
-                id={id}
-                rows={rows}
-                name={name} 
-                value={value}
-                disabled={disabled}
-                maxLength={maxLength}
-                placeholder={placeholder}
-                onChange={onChange}
-                className={cn(
-                    "text-sm p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-primary focus:border-transparent",
-                    className,
-                    error && "border-red-500"
-                )}
-            />
-            <ErrorMessage
-                show={error}
-                message={message}
-                className="mt-1"
-            />
-        </div>
-    )
-}
+const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
+    ({ id, label, name, className, error, message, ...rest }, ref) => {
+        return (
+            <div className={"flex flex-col gap-1 w-full"}>
+                <label 
+                    htmlFor={id} 
+                    className={cn(`text-sm text-dark font-semibold`, className)}
+                >
+                    {label}
+                </label>
+                <textarea 
+                    ref={ref}
+                    id={id}
+                    name={name} 
+                    className={cn(
+                        "text-sm p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-primary focus:border-transparent",
+                        className,
+                        error && "border-red-500"
+                    )}
+                    {...rest}
+                />
+                <ErrorMessage show={error} message={message} />
+            </div>
+        );
+    }
+);
+
+TextArea.displayName = "TextArea";
+export default TextArea;
