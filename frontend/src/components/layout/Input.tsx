d4b9cc0e800084ee3bf0d2952cfefa61ac42f07e
Walker-Gideon <gideonawortwe3@gmail.com>
2026-05-16 19:25:33 +0000

    Code refactoring in the Input component

--- frontend/src/components/layout/Input.tsx
+++ frontend/src/components/layout/Input.tsx
@@ -1,3 +1,4 @@
+import { forwardRef } from "react";
 import { cn } from "@/lib/utils";
 
 import Container from "./Container";
@@ -5,28 +6,30 @@ import ErrorMessage from "./ErrorMessage";
 
 import type { InputProps } from "./interfaces";
 
+const Input = forwardRef<HTMLInputElement, InputProps>(
+    ({ id, label, name, type, className, error, message, priority, style, ...rest }, ref) => {
+        return (
+            <Container variant="div" className={cn(priority ? "flex items-center gap-1" : "flex flex-col gap-1")}>
+                <label 
+                    htmlFor={id} 
+                    className={cn("text-sm text-dark", priority ? "font-regular text-gray-400" : "font-semibold", className)}
+                >
+                    {label}
+                </label>
+                <input 
+                    ref={ref}
+                    type={type} 
+                    id={id} 
+                    name={name} 
+                    style={style}
+                    className={cn(`w-full text-sm px-2.5 py-1.5 rounded-md border`, className, error ? "border-red-500" : "border-gray-300", priority ? `cursor-pointer` : "focus:outline-none focus:ring-1 focus:ring-primary focus:border-transparent")} 
+                    {...rest}
+                />
+                <ErrorMessage show={error} message={message} />
+            </Container>
+        );
+    }
+);
 
-export default function Input({ id, label, name, type, value, onChange, className, error, message, placeholder, disabled, priority, style }: InputProps) {
-    return (
-        <Container variant="div" className={cn(priority ? "flex items-center gap-1" : "flex flex-col gap-1")}>
-            <label 
-                htmlFor={id} 
-                className={cn("text-sm text-dark", priority ? "font-regular text-gray-400" : "font-semibold", className)}
-            >
-                {label}
-            </label>
-            <input 
-                type={type} 
-                id={id} 
-                name={name} 
-                value={value} 
-                disabled={disabled}
-                onChange={onChange} 
-                style={style}
-                className={cn(`w-full text-sm px-2.5 py-1.5 rounded-md border`, className, error ? "border-red-500" : "border-gray-300", priority ? `cursor-pointer` : "focus:outline-none focus:ring-1 focus:ring-primary focus:border-transparent")} 
-                placeholder={placeholder} 
-            />
-            <ErrorMessage show={error} message={message} />
-        </Container>
-    )
-}
+Input.displayName = "Input";
+export default Input;
