2205593aaca9ede4f882bf658f4b96a3e8d3d4ab
Walker-Gideon <gideonawortwe3@gmail.com>
2026-05-17 19:30:50 +0000

    Update the DashboardComplete component to display the complete cards from the backend

--- frontend/src/components/pages/dashboard/components/DashboardCompleted.tsx
+++ frontend/src/components/pages/dashboard/components/DashboardCompleted.tsx
@@ -7,28 +7,39 @@ import Container from "@/components/layout/Container";
 import Conditional from "@/components/layout/Conditional";
 import Information from "@/components/layout/Information";
 
+import { useGetCompletedTodos } from "@/components/hooks/useGetCompletedTodos";
+import type { Task } from "@/api/todos";
+
 export default function DashboardCompleted() {
+    const { completedTodos, isLoading, error } = useGetCompletedTodos();
+
     return (
         <ShadowBox
-            className={"px-4 md:px-6 flex flex-col min-h-0 max-h-[500px] md:max-h-none md:flex-1 mb-4 md:mb-0"}
+            className={"px-4 md:px-6 flex flex-col min-h-[300px] max-h-[500px] md:min-h-0 md:max-h-none md:flex-1 mb-4 md:mb-0"}
         >
             <SubHeading
                 icon={<TbClipboardCheck size={24} />}
                 subheading="Completed Task"
             />
-
             <Container 
                 variant="main" 
                 className={"w-full mt-4 flex-1 min-h-0 flex flex-col gap-4 overflow-y-auto"}
             >
-                <Conditional condition={false}>
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
+                <Conditional condition={isLoading}>
+                    <Information value="Loading completed tasks..." />
                 </Conditional>
-                <Conditional condition={true}>
-                    <Information />
+                <Conditional condition={!!error}>
+                    <Information value="Error while loading completed tasks." />
+                </Conditional>
+
+                <Conditional condition={(!completedTodos || completedTodos.length === 0) && !isLoading}>
+                    <Information value="No completed tasks." />
+                </Conditional>
+                
+                <Conditional condition={!!completedTodos && completedTodos.length > 0}>
+                    {completedTodos?.map((task: Task) => (
+                        <Card key={task._id} task={task} />
+                    ))}
                 </Conditional>
             </Container>
         </ShadowBox>
