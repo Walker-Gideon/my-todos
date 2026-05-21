ae6ef877bf60f096bbb76cbb4b49b02f155ca452
Walker-Gideon <gideonawortwe3@gmail.com>
2026-05-21 17:40:05 +0000

    Update in the dashboard index componen, but bug

--- frontend/src/components/pages/dashboard/index.tsx
+++ frontend/src/components/pages/dashboard/index.tsx
@@ -2,75 +2,97 @@ import { useState } from "react";
 
 import Container from "@/components/layout/Container";
 import DashboardTodos from "./components/DashboardTodos";
+import Conditional from "@/components/layout/Conditional";
 import DashboardHeader from "./components/DashboardHeader";
 import DashboardStatus from "./components/DashboardStatus";
 import DashboardCompleted from "./components/DashboardCompleted";
 import CreateTaskModal from "@/components/layout/CreateTaskModal";
 import DashboardInviteModal from "./components/DashboardInviteModal";
+import DashboardCardContent from "./components/DashboardCardContent";
 
 import type { Task } from "@/api/todos";
 
 export default function Dashboard() {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [taskToEdit, setTaskToEdit] = useState<Task | null>(null);
-    const [isInviteModalOpen, setIsInviteModalOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isContentOpen, setIsContentOpen] = useState(false);
+  const [contentId, setContentId] = useState<string | null>(null);
+  const [taskToEdit, setTaskToEdit] = useState<Task | null>(null);
+  const [isInviteModalOpen, setIsInviteModalOpen] = useState(false);
 
-    function handleModal() {
-        setTaskToEdit(null);
-        setIsModalOpen(!isModalOpen);
-    }
+  console.log("🚀 ~ file: index.tsx:17 ~ Dashboard ~ contentId:", contentId);
 
-    function handleCloseModal() {
-        setTaskToEdit(null);
-        setIsModalOpen(false);
-    }
+  function handleModal() {
+    setTaskToEdit(null);
+    setIsModalOpen(!isModalOpen);
+  }
 
-    function handleOpenEditModal(task: Task) {
-        setTaskToEdit(task);
-        setIsModalOpen(true);
-    }
+  function handleCloseModal() {
+    setTaskToEdit(null);
+    setIsModalOpen(false);
+  }
 
-    function handleInviteModal() {
-        setIsInviteModalOpen(!isInviteModalOpen);
-    }
+  function handleOpenEditModal(task: Task) {
+    setTaskToEdit(task);
+    setIsModalOpen(true);
+  }
 
-    return (
+  function handleInviteModal() {
+    setIsInviteModalOpen(!isInviteModalOpen);
+  }
+
+  return (
+    <Container variant="main" className={"w-full h-full flex flex-col min-h-0"}>
+      <Conditional condition={!isContentOpen}>
+        <DashboardHeader onOpenInviteModal={handleInviteModal} />
         <Container
-            variant="main"
-            className={"w-full h-full flex flex-col min-h-0"}
+          variant="div"
+          className={
+            "w-full flex-1 min-h-0 flex flex-col md:flex-row gap-4 medium:p-4 md:border md:border-gray-300 overflow-y-auto md:overflow-hidden"
+          }
         >
-            <DashboardHeader onOpenInviteModal={handleInviteModal} />
-            <Container
-                variant="div"
-                className={"w-full flex-1 min-h-0 flex flex-col md:flex-row gap-4 medium:p-4 md:border md:border-gray-300 overflow-y-auto md:overflow-hidden"}
-            >
-                <Container 
-                    variant="div" 
-                    className={"w-full flex-none md:flex-1 min-w-0 flex flex-col order-2 md:order-1"}
-                >
-                    <DashboardTodos onOpenModal={handleModal} onEditTask={handleOpenEditModal} />
-                </Container>
-                <Container
-                    variant="div"
-                    className={"w-full flex-none md:flex-1 min-w-0 flex flex-col gap-4 order-1 md:order-2"}
-                >
-                    <DashboardStatus />
-                    <DashboardCompleted onEditTask={handleOpenEditModal} />
-                </Container>
-            </Container>
-
-            <CreateTaskModal
-                show={isModalOpen} 
-                onCloseModal={handleCloseModal} 
-                fristWord={taskToEdit ? "Edit Ta" : "Add New Ta"}
-                secondWord="sk"
-                taskToEdit={taskToEdit}
+          <Container
+            variant="div"
+            className={
+              "w-full flex-none md:flex-1 min-w-0 flex flex-col order-2 md:order-1"
+            }
+          >
+            <DashboardTodos
+              onOpenModal={handleModal}
+              isContentId={setContentId}
+              onContentOpen={setIsContentOpen}
+              onEditTask={handleOpenEditModal}
             />
-            <DashboardInviteModal 
-                show={isInviteModalOpen} 
-                onCloseModal={handleInviteModal} 
-            />
+          </Container>
+          <Container
+            variant="div"
+            className={
+              "w-full flex-none md:flex-1 min-w-0 flex flex-col gap-4 order-1 md:order-2"
+            }
+          >
+            <DashboardStatus />
+            <DashboardCompleted onEditTask={handleOpenEditModal} />
+          </Container>
         </Container>
-    )
-}
+      </Conditional>
 
+      <Conditional condition={isContentOpen}>
+        <DashboardCardContent
+          contentId={contentId}
+          onContentOpen={setIsContentOpen}
+        />
+      </Conditional>
+
+      <CreateTaskModal
+        show={isModalOpen}
+        onCloseModal={handleCloseModal}
+        fristWord={taskToEdit ? "Edit Ta" : "Add New Ta"}
+        secondWord="sk"
+        taskToEdit={taskToEdit}
+      />
+      <DashboardInviteModal
+        show={isInviteModalOpen}
+        onCloseModal={handleInviteModal}
+      />
+    </Container>
+  );
+}
