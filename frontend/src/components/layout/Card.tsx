6e8c8378d04c9a9627b6963ce51b88b9059152a6
Walker-Gideon <gideonawortwe3@gmail.com>
2026-05-26 22:53:15 +0000

    update: optional some of the props in the Card component

--- frontend/src/components/layout/Card.tsx
+++ frontend/src/components/layout/Card.tsx
@@ -12,8 +12,8 @@ interface TaskProps {
   onEdit: (id: string) => void;
   onUndo?: (id: string) => void;
   onComplete?: (id: string) => void;
-  onIsContentId: (id: string) => void;
-  onContentOpen: (open: boolean) => void;
+  onIsContentId?: (id: string) => void;
+  onContentOpen?: (open: boolean) => void;
   onDelete: (title: string, id: string) => void;
 }
 
