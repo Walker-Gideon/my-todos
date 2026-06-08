4a8f0c723bb175c1988affeb8e085b366a76b081
Walker-Gideon <gideonawortwe3@gmail.com>
2026-06-08 17:16:18 +0000

    adjust the styling in the SecondaryHeading component

--- frontend/src/components/layout/SecondaryHeading.tsx
+++ frontend/src/components/layout/SecondaryHeading.tsx
@@ -3,9 +3,9 @@ import Paragraph from "@/components/ui/Paragraph";
 
 export default function SecondaryHeading({ fristWord, secondWord }: { fristWord: string, secondWord: string }) {
     return (
-        <Paragraph className={"w-full flex items-center text-dark text-[0.9rem] font-semibold"}>
+        <Paragraph className={"w-full flex items-center text-dark text-lg font-semibold"}>
             <Span className={"underline decoration-secondary decoration-2 underline-offset-4"}>{fristWord}</Span>
-            <Span>{secondWord}</Span>
+            <Span className={"ml-1"}>{secondWord}</Span>
         </Paragraph>
     )
 }
