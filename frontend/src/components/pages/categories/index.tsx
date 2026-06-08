9301262e7e5b5b4c067659fca670fa578d986cbc
Walker-Gideon <gideonawortwe3@gmail.com>
2026-06-08 17:46:02 +0000

    feat: add Categories page

--- frontend/src/components/pages/categories/index.tsx
+++ frontend/src/components/pages/categories/index.tsx
@@ -1,5 +1,27 @@
+import Button from "@/components/ui/Button";
+import ShadowBox from "@/components/layout/ShadowBox";
+import Container from "@/components/layout/Container";
+import CategoryStatus from "./components/CategoryStatus";
+import CategoryPriority from "./components/CategoryPriority";
+import SecondaryHeading from "@/components/layout/SecondaryHeading";
+
 export default function Categories() {
     return (
-        <p>Categories</p>
+        <ShadowBox border={true} className={"px-4 md:px-6 flex flex-col min-h-120 max-h-160 md:min-h-120 md:max-h-none md:h-full mb-4 md:mb-0"}>
+            <Container variant="div">
+                <SecondaryHeading fristWord={"Task"} secondWord={" Categories"} />
+                <Button 
+                    ariaLabel="Add Category" 
+                    onClick={() => { }}
+                    className={"mt-4 button-secondary-styling"}
+                >
+                    Add Category
+                </Button>
+            </Container>
+            <Container variant="div" className={"mt-8 flex flex-col gap-4"}>
+                <CategoryStatus />
+                <CategoryPriority />
+            </Container>
+        </ShadowBox>
     )
 }
