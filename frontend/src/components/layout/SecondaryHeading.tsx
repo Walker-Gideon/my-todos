61b41ea90b9f07e34e16d8317b890e21f6cbb913
Walker-Gideon <gideonawortwe3@gmail.com>
2026-06-08 17:22:16 +0000

    update in the SecondaryHeading component

--- frontend/src/components/layout/SecondaryHeading.tsx
+++ frontend/src/components/layout/SecondaryHeading.tsx
@@ -1,9 +1,9 @@
 import Span from "@/components/ui/Span";
 import Paragraph from "@/components/ui/Paragraph";
 
-export default function SecondaryHeading({ fristWord, secondWord }: { fristWord: string, secondWord: string }) {
+export default function SecondaryHeading({ fristWord, secondWord, textSize="text-lg" }: { fristWord: string, secondWord: string, textSize?: string }) {
     return (
-        <Paragraph className={"w-full flex items-center text-dark text-lg font-semibold"}>
+        <Paragraph className={`w-full flex items-center text-dark font-semibold ${textSize}`}>
             <Span className={"underline decoration-secondary decoration-2 underline-offset-4"}>{fristWord}</Span>
             <Span className={"ml-1"}>{secondWord}</Span>
         </Paragraph>
