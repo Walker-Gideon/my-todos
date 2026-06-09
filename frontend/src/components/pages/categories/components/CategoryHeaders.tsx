66b31a82a9e4acb862c1c9a152867857ec01915f
Walker-Gideon <gideonawortwe3@gmail.com>
2026-06-09 22:17:42 +0000

    feat: update CategoryHeaders component for improved layout and spacing

--- frontend/src/components/pages/categories/components/CategoryHeaders.tsx
+++ frontend/src/components/pages/categories/components/CategoryHeaders.tsx
@@ -6,25 +6,36 @@ import Container from "@/components/layout/Container";
 import SecondaryHeading from "@/components/layout/SecondaryHeading";
 
 interface CategoryHeadersProps {
-    onOpenModal: () => void;
-    heading?: string;
+  onOpenModal: () => void;
+  heading?: string;
 }
 
-export default function CategoryHeaders({ onOpenModal, heading }: CategoryHeadersProps) {
-    return (
-        <Container variant="div" className={"flex items-center gap-2"}>
-            <SecondaryHeading fristWord="Task" secondWord={heading} textSize="text-sm" />
-            <Button 
-                variant="text"
-                ariaLabel="Add Task Status"
-                onClick={onOpenModal}
-                className={"group w-auto flex items-center gap-1 font-regular text-gray hover:text-secondary transition-primary whitespace-nowrap"}
-            >
-                <TbPlus
-                    className={"w-4 h-4 group-hover:rounded-full group-hover:bg-secondary group-hover:text-col-white transition-primary text-secondary"}
-                />
-                <Span>Add Task {heading}</Span>
-            </Button>
-        </Container>
-    )
+export default function CategoryHeaders({
+  onOpenModal,
+  heading,
+}: CategoryHeadersProps) {
+  return (
+    <Container variant="div" className={"flex items-center gap-2 mb-3"}>
+      <SecondaryHeading
+        fristWord="Task"
+        secondWord={heading ?? ""}
+        textSize="text-sm"
+      />
+      <Button
+        variant="text"
+        ariaLabel="Add Task Status"
+        onClick={onOpenModal}
+        className={
+          "group w-auto flex items-center gap-1 font-regular text-gray hover:text-secondary transition-primary whitespace-nowrap"
+        }
+      >
+        <TbPlus
+          className={
+            "w-4 h-4 group-hover:rounded-full group-hover:bg-secondary group-hover:text-col-white transition-primary text-secondary"
+          }
+        />
+        <Span>Add Task {heading ?? ""}</Span>
+      </Button>
+    </Container>
+  );
 }
