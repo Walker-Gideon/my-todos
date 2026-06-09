0e02d2310a75c3b87d17560615e4619a4250276f
Walker-Gideon <gideonawortwe3@gmail.com>
2026-06-09 22:17:33 +0000

    feat: implement CategoryPriority component with task priority display

--- frontend/src/components/pages/categories/components/CategoryPriority.tsx
+++ frontend/src/components/pages/categories/components/CategoryPriority.tsx
@@ -1,16 +1,21 @@
-import Button from "@/components/ui/Button";
+import CategoryTable from "./CategoryTable";
 import Container from "@/components/layout/Container";
-import ShadowBox from "@/components/layout/ShadowBox";
 import CategoryHeaders from "@/components/pages/categories/components/CategoryHeaders";
 
+const priorityRows = [
+  { id: 1, name: "Extreme" },
+  { id: 2, name: "Medium" },
+  { id: 3, name: "Low" },
+];
+
 export default function CategoryPriority() {
-    //min-h-120 max-h-160 md:min-h-120 md:max-h-none md:h-full mb-4 md:mb-0
-    return (
-        <Container variant="div">
-            <CategoryHeaders heading="Priority" onOpenModal={() => {}} />
-            <ShadowBox border={true} className={"px-4 md:px-6 flex flex-col"}>
-                <p>Tesk Priority here</p>
-            </ShadowBox>
-        </Container>
-    )
+  return (
+    <Container variant="div" className={"flex flex-col"}>
+      <CategoryHeaders heading="Priority" onOpenModal={() => {}} />
+      <CategoryTable
+        headers={["SN", "Task Priority", "Action"]}
+        statusRows={priorityRows}
+      />
+    </Container>
+  );
 }
