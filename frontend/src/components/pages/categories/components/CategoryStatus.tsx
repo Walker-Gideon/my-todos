c4ecb9b4cc145cf0f2cdd756b5d8d3b7d717aef2
Walker-Gideon <gideonawortwe3@gmail.com>
2026-06-15 21:05:14 +0000

    feat: add state from the context to CategoryStatus to handle the category modal

--- frontend/src/components/pages/categories/components/CategoryStatus.tsx
+++ frontend/src/components/pages/categories/components/CategoryStatus.tsx
@@ -2,19 +2,23 @@ import CategoryTable from "./CategoryTable";
 import Container from "@/components/layout/Container";
 import CategoryHeaders from "@/components/pages/categories/components/CategoryHeaders";
 
-const statusRows = [
+import { useCategoryContext } from "../context/CategoryContext";
+
+const STATUS_ROWS = [
   { id: 1, name: "Completed" },
   { id: 2, name: "In Progress" },
   { id: 3, name: "Not Started" },
 ];
 
 export default function CategoryStatus() {
+  const { setActiveCategoryModal } = useCategoryContext();
+  
   return (
     <Container variant="div" className={"flex flex-col "}>
-      <CategoryHeaders heading="Status" onOpenModal={() => {}} />
+      <CategoryHeaders heading="Status" onOpenModal={() => setActiveCategoryModal("Status")} />
       <CategoryTable
         headers={["SN", "Task Status", "Action"]}
-        statusRows={statusRows}
+        statusRows={STATUS_ROWS}
       />
     </Container>
   );
