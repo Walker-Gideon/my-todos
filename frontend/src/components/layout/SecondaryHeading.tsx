ec3403a75f86f68f9125ca0ed27c76c663d5e8d6
Walker-Gideon <gideonawortwe3@gmail.com>
2026-06-15 22:01:57 +0000

    refactor: update the SecondaryHeading interface to cater for optional secondword

--- frontend/src/components/layout/SecondaryHeading.tsx
+++ frontend/src/components/layout/SecondaryHeading.tsx
@@ -1,7 +1,7 @@
 import Span from "@/components/ui/Span";
 import Paragraph from "@/components/ui/Paragraph";
 
-export default function SecondaryHeading({ fristWord, secondWord, textSize="text-lg" }: { fristWord: string, secondWord: string, textSize?: string }) {
+export default function SecondaryHeading({ fristWord, secondWord, textSize="text-lg" }: { fristWord: string, secondWord?: string, textSize?: string }) {
     return (
         <Paragraph className={`w-full flex items-center text-dark font-semibold ${textSize}`}>
             <Span className={"underline decoration-secondary decoration-2 underline-offset-4"}>{fristWord}</Span>
