9f4b885666b439c0d3e92f6513c8edaa48bd465b
Walker-Gideon <gideonawortwe3@gmail.com>
2026-06-18 14:35:25 +0000

    refactor: update the FormRow code to imporove props

---
+++ frontend/src/components/layout/FormRow.tsx
@@ -0,0 +1,28 @@
+import Span from "@/components/ui/Span";
+import Container from "@/components/layout/Container";
+
+export default function FormRow({
+  children,
+  errorsField,
+  errorMessage,
+}: {
+  children: React.ReactNode;
+  errorsField?: any;
+  errorMessage?: string;
+}) {
+  return (
+    <Container variant="div" className={"flex flex-col gap-1"}>
+      <Container
+        variant="div"
+        className={
+          "flex items-center border border-border-primary rounded-md px-3 text-sm hover:border-primary-hover focus-within:border-primary focus-within:ring-1 focus-within:ring-primary/20 transition-all"
+        }
+      >
+        {children}
+      </Container>
+      {errorsField && (
+        <Span className="text-xs text-red-500 ml-1">{errorMessage}</Span>
+      )}
+    </Container>
+  );
+}
