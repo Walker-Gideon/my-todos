4d9303c92be0ef765c6792f641da54a7d1994ddc
Walker-Gideon <gideonawortwe3@gmail.com>
2026-06-18 21:33:20 +0000

    style: styling adjustment made in the AppLayout compnent

--- frontend/src/components/pages/index.tsx
+++ frontend/src/components/pages/index.tsx
@@ -7,55 +7,52 @@ import Backdrop from "@/components/layout/Backdrop";
 import Container from "@/components/layout/Container";
 import Navigation from "@/components/layout/Navigation";
 
-
 export default function AppLayout() {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    function handleMenu() {
-        setIsMenuOpen(!isMenuOpen);
-    }
+  function handleMenu() {
+    setIsMenuOpen(!isMenuOpen);
+  }
 
-    useEffect(() => {
-        const handleResize = () => {
-            if (window.innerWidth >= 768) {
-                setIsMenuOpen(false);
-            }
-        };
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 768) {
+        setIsMenuOpen(false);
+      }
+    };
 
-        window.addEventListener("resize", handleResize);
-        return () => window.removeEventListener("resize", handleResize);
-    }, []);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
-    return (
-        <Container 
-            container="div" 
-            className={"h-screen flex flex-col overflow-hidden"}
+  return (
+    <Container
+      container="div"
+      className={"h-screen flex flex-col overflow-hidden"}
+    >
+      <Header menuOpen={isMenuOpen} onMenuClick={handleMenu} />
+
+      <Container
+        variant="div"
+        className={"w-full flex-1 flex min-h-0 overflow-hidden"}
+      >
+        <Navigation />
+        <Container
+          variant="main"
+          // overflow-y-auto md:overflow-hidden
+          className={"flex-1 min-h-0 mx-4 md:mx-8 pb-2 overflow-hidden"}
         >
-            <Header 
-                menuOpen={isMenuOpen} 
-                onMenuClick={handleMenu} 
-            />
-
-            <Container 
-                variant="div" 
-                className={"w-full flex-1 flex min-h-0 overflow-hidden"}
-            >
-                <Navigation />
-                <Container 
-                    variant="main" 
-                    className={"flex-1 min-h-0 mx-4 md:mx-8 overflow-y-auto md:overflow-hidden"}
-                >
-                    <Outlet />
-                </Container>
-            </Container>
-
-            <AnimatePresence>
-                {isMenuOpen && (
-                    <Backdrop onClick={handleMenu} show={isMenuOpen}>
-                        <Navigation show={isMenuOpen} onClick={handleMenu} />
-                    </Backdrop>
-                )}
-            </AnimatePresence>
+          <Outlet />
         </Container>
-    )
+      </Container>
+
+      <AnimatePresence>
+        {isMenuOpen && (
+          <Backdrop onClick={handleMenu} show={isMenuOpen}>
+            <Navigation show={isMenuOpen} onClick={handleMenu} />
+          </Backdrop>
+        )}
+      </AnimatePresence>
+    </Container>
+  );
 }
