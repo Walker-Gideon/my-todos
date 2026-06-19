ad6a69ed3c7fa19d457cf5b76593f19cbfcf3edf
Walker-Gideon <gideonawortwe3@gmail.com>
2026-06-19 12:57:53 +0000

    styling: Ui and styling fix in the Vital section

--- frontend/src/components/pages/vital/index.tsx
+++ frontend/src/components/pages/vital/index.tsx
@@ -1,47 +1,44 @@
-import Container from "@/components/layout/Container";
 import VitalTaskDisplay from "./components/VitalTaskDisplay";
 import VitalTaskContent from "./components/VitalTaskContent";
+import AnimationWraper from "@/components/layout/AnimationWraper";
 import CreateTaskModal from "@/components/layout/CreateTaskModal";
 
 import { useTaskParams } from "@/components/hooks/useTaskParams";
 
 export default function VitalTask() {
-    const { 
-        isModalOpen,
-        taskToEdit,
-        contentId,
-        isContentOpen,
-        handleIsContentId,
-        handleIsContentOpen,
-        handleOpenEditModal,
-        handleCloseModal, 
-    } = useTaskParams();
+  const {
+    isModalOpen,
+    taskToEdit,
+    contentId,
+    isContentOpen,
+    handleIsContentId,
+    handleIsContentOpen,
+    handleOpenEditModal,
+    handleCloseModal,
+  } = useTaskParams();
 
-    return (
-        <Container 
-            variant="main" 
-            className={"w-full h-full flex flex-col md:flex-row gap-4 min-h-0 overflow-y-auto md:overflow-hidden"}
-        >
-            <VitalTaskDisplay 
-                contentId={contentId}
-                isPanelOpen={isContentOpen}
-                onIsContentId={handleIsContentId}
-                onContentOpen={handleIsContentOpen} 
-                onOpenEditModal={handleOpenEditModal} 
-            />
-            <VitalTaskContent 
-                contentId={contentId}
-                isPanelOpen={isContentOpen}
-                onEditTask={handleOpenEditModal}
-                onContentOpen={handleIsContentOpen} 
-            />
-            <CreateTaskModal 
-                secondWord="sk"
-                fristWord="Edit Ta"
-                show={isModalOpen}
-                taskToEdit={taskToEdit}
-                onCloseModal={handleCloseModal}
-            />
-        </Container>
-    )
+  return (
+    <AnimationWraper className={"w-full h-full min-h-0 flex flex-1 md:gap-4"}>
+      <VitalTaskDisplay
+        contentId={contentId}
+        isPanelOpen={isContentOpen}
+        onIsContentId={handleIsContentId}
+        onContentOpen={handleIsContentOpen}
+        onOpenEditModal={handleOpenEditModal}
+      />
+      <VitalTaskContent
+        contentId={contentId}
+        isPanelOpen={isContentOpen}
+        onEditTask={handleOpenEditModal}
+        onContentOpen={handleIsContentOpen}
+      />
+      <CreateTaskModal
+        secondWord="sk"
+        fristWord="Edit Ta"
+        show={isModalOpen}
+        taskToEdit={taskToEdit}
+        onCloseModal={handleCloseModal}
+      />
+    </AnimationWraper>
+  );
 }
