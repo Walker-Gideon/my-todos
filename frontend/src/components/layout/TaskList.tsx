925f0d0bf5a990afb8bdbfef50b70611701af3db
Walker-Gideon <gideonawortwe3@gmail.com>
2026-06-19 00:13:53 +0000

    style: adjust the stying in the TaskList component

--- frontend/src/components/layout/TaskList.tsx
+++ frontend/src/components/layout/TaskList.tsx
@@ -14,78 +14,89 @@ import { useUpdateTask } from "@/components/hooks/useUpdateTask";
 import { useGetTodosTask } from "@/components/hooks/useGetTodosTask";
 
 interface TaskListProps {
-    todosTasks: Task[];
-    contentId: string;
-    fristWord: string;
-    secondWord: string;
-    isPanelOpen?: boolean;
-    onIsContentId: (id: string) => void;
-    onOpenEditModal: (task: Task) => void;
-    onContentOpen: (open: boolean) => void;
+  todosTasks: Task[];
+  contentId: string;
+  fristWord: string;
+  secondWord: string;
+  isPanelOpen?: boolean;
+  onIsContentId: (id: string) => void;
+  onOpenEditModal: (task: Task) => void;
+  onContentOpen: (open: boolean) => void;
 }
 
-export default function TaskList({ todosTasks, fristWord, secondWord, ...rest }: TaskListProps) {
-    const { contentId, onIsContentId, onContentOpen, onOpenEditModal } = rest;
+export default function TaskList({
+  todosTasks,
+  fristWord,
+  secondWord,
+  ...rest
+}: TaskListProps) {
+  const { contentId, onIsContentId, onContentOpen, onOpenEditModal } = rest;
 
-    const { updateTask } = useUpdateTask();
-    const { isLoading, error } = useGetTodosTask();
-    const { deleteTodo, isPending } = useDeleteTodo();
+  const { updateTask } = useUpdateTask();
+  const { isLoading, error } = useGetTodosTask();
+  const { deleteTodo, isPending } = useDeleteTodo();
 
-    const [isDeleteTitle, setIsDeleteTitle] = useState("");
-    const [isDeleteModalOpen, setIsDeleteModalOpen] = useState("");
+  const [isDeleteTitle, setIsDeleteTitle] = useState("");
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState("");
 
-    return (
-        <ShadowBox border={true} className={"px-4 md:px-6 flex flex-col min-h-120 max-h-160 md:min-h-120 md:max-h-none md:h-full mb-4 md:mb-0"}>
-            <SecondaryHeading fristWord={fristWord} secondWord={secondWord} />
-            <Container 
-                variant="main" 
-                className={"w-full mt-4 flex-1 min-h-0 flex flex-col gap-4 overflow-y-auto"}
-            >
-                <Conditional condition={todosTasks?.length > 0}>  
-                    {todosTasks?.map((task: Task) => (
-                        <Card 
-                            key={task._id} 
-                            task={task} 
-                            onIsContentId={onIsContentId}
-                            onContentOpen={onContentOpen}
-                            isSelected={contentId === task._id}
-                            onEdit={() => onOpenEditModal(task)} 
-                            onDelete={(title, id) => {
-                                setIsDeleteModalOpen(id);
-                                setIsDeleteTitle(title);
-                            }} 
-                            onUndo={(id) => updateTask({ id, data: { isVital: false } })} 
-                            undoText="vital task"
-                        />
-                    ))}
-                </Conditional>
-                <Conditional condition={todosTasks?.length === 0}>
-                    <Information />
-                </Conditional>
-                <Conditional condition={isLoading}>
-                    <Information value="Loading vital tasks..." />
-                </Conditional>
-                <Conditional condition={!!error}>
-                    <Information value="Error while loading vital tasks." />
-                </Conditional>
-            </Container>
-            <ConfirmDelete
-                resourceName={isDeleteTitle}
-                onConfirm={() => {
-                    deleteTodo(isDeleteModalOpen, {
-                        onSuccess: () => {
-                            setIsDeleteModalOpen("");
-                            setIsDeleteTitle("");
-                        }
-                    });
-                }}
-                onCloseModal={() => {
-                    setIsDeleteModalOpen("");
-                    setIsDeleteTitle("");
-                }}
-                disabled={isPending}
-                show={!!isDeleteModalOpen}
+  return (
+    <ShadowBox
+      className={
+        "px-1 md:p-4 flex flex-col min-h-120 max-h-160 md:min-h-120 md:max-h-none md:h-full md:border md:border-gray-300 md:rounded-xl"
+      }
+    >
+      <SecondaryHeading fristWord={fristWord} secondWord={secondWord} />
+      <Container
+        variant="main"
+        className={
+          "w-full mt-4 flex flex-1 flex-col gap-4 h-full min-h-0 overflow-y-auto"
+        }
+      >
+        <Conditional condition={todosTasks?.length > 0}>
+          {todosTasks?.map((task: Task) => (
+            <Card
+              key={task._id}
+              task={task}
+              onIsContentId={onIsContentId}
+              onContentOpen={onContentOpen}
+              isSelected={contentId === task._id}
+              onEdit={() => onOpenEditModal(task)}
+              onDelete={(title, id) => {
+                setIsDeleteModalOpen(id);
+                setIsDeleteTitle(title);
+              }}
+              onUndo={(id) => updateTask({ id, data: { isVital: false } })}
+              undoText="vital task"
             />
-        </ShadowBox>
-    )
+          ))}
+        </Conditional>
+        <Conditional condition={todosTasks?.length === 0}>
+          <Information />
+        </Conditional>
+        <Conditional condition={isLoading}>
+          <Information value="Loading vital tasks..." />
+        </Conditional>
+        <Conditional condition={!!error}>
+          <Information value="Error while loading vital tasks." />
+        </Conditional>
+      </Container>
+      <ConfirmDelete
+        resourceName={isDeleteTitle}
+        onConfirm={() => {
+          deleteTodo(isDeleteModalOpen, {
+            onSuccess: () => {
+              setIsDeleteModalOpen("");
+              setIsDeleteTitle("");
+            },
+          });
+        }}
+        onCloseModal={() => {
+          setIsDeleteModalOpen("");
+          setIsDeleteTitle("");
+        }}
+        disabled={isPending}
+        show={!!isDeleteModalOpen}
+      />
+    </ShadowBox>
+  );
 }
