6826dbb94d48f341a10608f04af645da72011a4a
Walker-Gideon <gideonawortwe3@gmail.com>
2026-06-19 00:15:12 +0000

    refactor: update the task index file with the anmation wraper

--- frontend/src/components/pages/task/index.tsx
+++ frontend/src/components/pages/task/index.tsx
@@ -2,46 +2,49 @@ import TaskDisplay from "./components/TaskDisplay";
 import TaskContent from "./components/TaskContent";
 import Container from "@/components/layout/Container";
 import CreateTaskModal from "@/components/layout/CreateTaskModal";
+import AnimationWraper from "@/components/layout/AnimationWraper";
 
 import { useTaskParams } from "@/components/hooks/useTaskParams";
 
 export default function Task() {
-    const {
-        isModalOpen,
-        taskToEdit,
-        contentId,
-        isContentOpen,
-        handleIsContentId,
-        handleIsContentOpen,
-        handleOpenEditModal,
-        handleCloseModal
-    } = useTaskParams();
-    
-    return (
-        <Container 
-            variant="main" 
-            className={"w-full h-full flex flex-col md:flex-row gap-4 min-h-0 overflow-y-auto md:overflow-hidden"}
-        >
-            <TaskDisplay 
-                contentId={contentId}
-                isPanelOpen={isContentOpen}
-                onIsContentId={handleIsContentId}
-                onContentOpen={handleIsContentOpen} 
-                onOpenEditModal={handleOpenEditModal}  
-            />
-            <TaskContent 
-                contentId={contentId}
-                isPanelOpen={isContentOpen}
-                onEditTask={handleOpenEditModal}
-                onContentOpen={handleIsContentOpen}
-            />
-            <CreateTaskModal 
-                secondWord="sk"
-                fristWord="Edit Ta"
-                show={isModalOpen}
-                taskToEdit={taskToEdit}
-                onCloseModal={handleCloseModal}
-            />
-        </Container>
-    )
+  const {
+    isModalOpen,
+    taskToEdit,
+    contentId,
+    isContentOpen,
+    handleIsContentId,
+    handleIsContentOpen,
+    handleOpenEditModal,
+    handleCloseModal,
+  } = useTaskParams();
+
+  return (
+    <AnimationWraper>
+      <Container
+        variant="main"
+        className={"w-full h-full min-h-0 flex flex-col md:flex-row gap-4"}
+      >
+        <TaskDisplay
+          contentId={contentId}
+          isPanelOpen={isContentOpen}
+          onIsContentId={handleIsContentId}
+          onContentOpen={handleIsContentOpen}
+          onOpenEditModal={handleOpenEditModal}
+        />
+        <TaskContent
+          contentId={contentId}
+          isPanelOpen={isContentOpen}
+          onEditTask={handleOpenEditModal}
+          onContentOpen={handleIsContentOpen}
+        />
+        <CreateTaskModal
+          secondWord="sk"
+          fristWord="Edit Ta"
+          show={isModalOpen}
+          taskToEdit={taskToEdit}
+          onCloseModal={handleCloseModal}
+        />
+      </Container>
+    </AnimationWraper>
+  );
 }
