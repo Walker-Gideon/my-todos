1769824efefe62cd9a608bba0f1e32194b3509ea
Walker-Gideon <gideonawortwe3@gmail.com>
2026-06-22 22:05:57 +0000

    refactor: fix in the pages interface component

--- frontend/src/components/ui/interfaces.ts
+++ frontend/src/components/ui/interfaces.ts
@@ -1,38 +1,38 @@
 import React from "react";
 
 export interface UiProps extends React.HTMLAttributes<HTMLHeadingElement> {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export interface HeadingsProps extends UiProps {
-    variant: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+  variant: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 }
 
 export interface InputProps extends React.HTMLAttributes<HTMLInputElement> {
-    type: string;
-    disabled?: boolean;
-    placeholder?: string;
-    className?: string;
-    defaultStyling?: boolean;
+  type: string;
+  disabled?: boolean;
+  placeholder?: string;
+  className?: string;
+  defaultStyling?: boolean;
 }
 
 export interface SpanProps extends React.HTMLAttributes<HTMLSpanElement> {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export interface LabelProps extends React.HTMLAttributes<HTMLLabelElement> {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
-    children: React.ReactNode;
-    ariaLabel?: string;
-    disabled?: boolean;
-    type?: "submit" | "reset";
-    variant?: "primary" | "outline" | "text";
+  children: React.ReactNode;
+  ariaLabel?: string;
+  disabled?: boolean;
+  type?: "submit" | "reset";
+  variant?: "primary" | "outline" | "text";
 }
 
 export interface ParagraphProps extends React.HTMLAttributes<HTMLParagraphElement> {
-    children: React.ReactNode;
-    variant?: "small" | "medium" | "large";
+  children: React.ReactNode;
+  variant?: "small" | "medium" | "large" | undefined;
 }
