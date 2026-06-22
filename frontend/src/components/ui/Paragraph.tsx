69427a583c92b1ae295a6bfe4f13092423c96240
Walker-Gideon <gideonawortwe3@gmail.com>
2026-06-22 22:07:31 +0000

    refactor: fix type bug in the Paragraph component

--- frontend/src/components/ui/Paragraph.tsx
+++ frontend/src/components/ui/Paragraph.tsx
@@ -1,15 +1,20 @@
 import type { ParagraphProps } from "./interfaces";
 
-export default function Paragraph({ children, className, variant, ...rest }: ParagraphProps) {
-    const defaultStyling = {
-        small: "text-sm",
-        medium: "text-base",
-        large: "text-lg"
-    }
-    
-    return (
-        <p className={`${defaultStyling[variant]} ${className}`} {...rest}>
-            {children}
-        </p>
-    )
+export default function Paragraph({
+  children,
+  className,
+  variant = "medium",
+  ...rest
+}: ParagraphProps) {
+  const defaultStyling = {
+    small: "text-sm",
+    medium: "text-base",
+    large: "text-lg",
+  };
+
+  return (
+    <p className={`${defaultStyling[variant]} ${className}`} {...rest}>
+      {children}
+    </p>
+  );
 }
