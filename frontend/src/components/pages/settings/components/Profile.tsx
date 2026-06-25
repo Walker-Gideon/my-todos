296f03b01cc4df1beab1628f3c74dd38513a50da
Walker-Gideon <gideonawortwe3@gmail.com>
2026-06-25 22:46:19 +0000

    refactor: update the profile component with uploading image in the profile.

--- frontend/src/components/pages/settings/components/Profile.tsx
+++ frontend/src/components/pages/settings/components/Profile.tsx
@@ -1,23 +1,68 @@
+import { useState } from "react";
+import { MdAddPhotoAlternate } from "react-icons/md";
+
 import Span from "@/components/ui/Span";
 import Paragraph from "@/components/ui/Paragraph";
 import Container from "@/components/layout/Container";
 
 import { useUserProfile } from "@/components/hooks/useUserProfile";
 
-export default function Profile({ primary = true }: { primary?: boolean }) {
+export default function Profile({
+  primary = true,
+  showBtn = false,
+}: {
+  primary?: boolean;
+  showBtn?: boolean;
+}) {
   const { name, capName, email } = useUserProfile();
+
+  const [preview, setPreview] = useState<string | null>(null);
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+
+    if (!file) return;
+    setPreview(URL.createObjectURL(file));
+  };
 
   return (
     <Container
       variant="div"
       className={`my-8 flex md:flex-row items-center gap-2 md:gap-4 ${primary ? "flex-col" : "flex-row"}`}
     >
-      <Container variant="div" className={"flex shrink-0"}>
-        <Span
-          className={`rounded-full bg-dark flex items-center justify-center text-white font-bold  ${primary ? "w-30 h-30 text-3xl" : "w-18 h-18 text-2xl"}`}
-        >
-          {capName}
-        </Span>
+      <Container variant="div" className={"relative flex shrink-0"}>
+        {preview ? (
+          <img
+            src={preview}
+            alt="preview"
+            className={`rounded-full border-2 border-white object-cover ${primary ? "w-30 h-30 text-3xl" : "w-18 h-18 text-2xl"}`}
+          />
+        ) : (
+          <Span
+            className={`rounded-full bg-dark flex items-center justify-center text-white font-bold  ${primary ? "w-30 h-30 text-3xl" : "w-18 h-18 text-2xl"}`}
+          >
+            {capName}
+          </Span>
+        )}
+
+        {showBtn && (
+          <label
+            htmlFor="profile-upload"
+            className={
+              "absolute right-0 bottom-0 flex items-center justify-center cursor-pointer h-7 w-7 rounded-full bg-btn-col border-2 border-white shadow-sm hover:opacity-90 transition-opacity"
+            }
+            title={preview ? "Change Photo" : "Add Photo"}
+          >
+            <MdAddPhotoAlternate size={16} className="text-white" />
+            <input
+              id="profile-upload"
+              type="file"
+              accept="image/*"
+              className="hidden"
+              onChange={handleImageChange}
+            />
+          </label>
+        )}
       </Container>
       <Container
         variant="div"
