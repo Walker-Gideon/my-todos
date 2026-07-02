b6eebeb154b4ad670ca6fadff98cce6a4a98ff4b
Walker-Gideon <gideonawortwe3@gmail.com>
2026-07-02 17:53:05 +0000

    fix: add profile query key to user profile to update the app or save and login

--- frontend/src/components/hooks/useUserProfile.ts
+++ frontend/src/components/hooks/useUserProfile.ts
@@ -1,10 +1,19 @@
-import { useAuth } from "@/context/useAuthContex";
+import { useQuery } from "@tanstack/react-query";
+
+import { getUserProfile } from "@/api/auth";
 
 export const useUserProfile = () => {
-  const { user } = useAuth();
+  const token = localStorage.getItem("token");
+
+  const { data: user } = useQuery({
+    queryKey: ["profile"],
+    queryFn: getUserProfile,
+    enabled: !!token,
+    select: (response) => response?.data ?? null,
+  });
 
   const { email, firstName, lastName, profileImageUrl, _id } = user ?? {};
-  const name = `${firstName} ${lastName}`;
+  const name = `${firstName || ""} ${lastName || ""}`.trim();
 
   const capitalizeName = (name?: string) => {
     if (!name) return "";
