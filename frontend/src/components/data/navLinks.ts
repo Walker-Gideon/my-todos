28c0b993b2f81edf2b647efee6cf9f1254cf5e25
Walker-Gideon <gideonawortwe3@gmail.com>
2026-07-16 23:44:30 +0000

    refactor: update the navLink data file with the heading texts

--- frontend/src/components/data/navLinks.ts
+++ frontend/src/components/data/navLinks.ts
@@ -11,25 +11,30 @@ export const navItems = [
     name: "Dashboard",
     href: "/dashboard",
     icon: TbLayoutDashboardFilled,
+    heading: "Dashboard",
   },
   {
     name: "Vital Task",
     href: "/vital-task",
     icon: RxExclamationMark,
+    heading: "To-Do",
   },
   {
     name: "My Task",
     href: "/my-task",
     icon: TbClipboardCheck,
+    heading: "To-Do",
   },
   {
     name: "Categories",
     href: "/categories",
     icon: TbList,
+    heading: "Categories",
   },
   {
     name: "Profile",
     href: "/profile",
     icon: TbUser,
+    heading: "Profile",
   },
 ];
