d0052ebafc6b187d20eb4fb70f5a340a1cc435f8
Walker-Gideon <gideonawortwe3@gmail.com>
2026-07-16 23:45:18 +0000

    refactor: add state to handle the change and update of the Header texts in the Header

--- frontend/src/context/GeneralContext.tsx
+++ frontend/src/context/GeneralContext.tsx
@@ -3,6 +3,8 @@ import React, { createContext, useState } from "react";
 type GeneralContextType = {
   query: string;
   setQuery: React.Dispatch<React.SetStateAction<string>>;
+  heading: string;
+  setHeading: React.Dispatch<React.SetStateAction<string>>;
 };
 
 const GeneralContext = createContext<GeneralContextType | undefined>(undefined);
@@ -12,10 +14,13 @@ export const GeneralProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const [query, setQuery] = useState("");
+  const [heading, setHeading] = useState("Dashboard");
 
   const value: GeneralContextType = {
     query,
     setQuery,
+    heading,
+    setHeading,
   };
 
   return (
