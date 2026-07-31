47be4b5d04c59391ac0a82a003f52fb8ed526ca0
Walker-Gideon <gideonawortwe3@gmail.com>
2026-07-31 21:15:33 +0000

    style: update DashboardStatus styling to correct alignment of each progress circle

--- frontend/src/components/pages/dashboard/components/DashboardStatus.tsx
+++ frontend/src/components/pages/dashboard/components/DashboardStatus.tsx
@@ -81,7 +81,7 @@ export default function DashboardStatus() {
         icon={<TbClipboardData size={24} />}
         subheading={"Task Status"}
       />
-      <div className={"flex items-center justify-between gap-2 mt-4"}>
+      <div className={"flex items-center justify-between xl:justify-center gap-2 xl:gap-12 mt-4"}>
         <CircleProgress
           percentage={completedPercentage}
           color="#22c55e"
